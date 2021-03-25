27e70ea9fd716c3b1eaec53d2c8e8f7e73a2a10d
khedermariem <kheder@DESKTOP-ADRH9K7.localdomain>
2021-03-25 20:34:59 +0100

    khedermariem added

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -3,6 +3,6 @@ class Tournament < ApplicationRecord
     has_many :teams, through: :TeamAndTournamentLinked
     has_many :matches
     def random_team
-        self.limit(2).offset (rand(@team)).first
+       (self.limit(2).offset (rand(@team)).first).to_i
     end
 end
